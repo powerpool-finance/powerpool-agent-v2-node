d516235aa13de681dfa603d64f75682317f146a8
defi-dev <defi.dev@protonmail.com>
2023-07-25 23:19:03 +0800

    move run logic from App.ts to Run.ts

--- app/App.ts
+++ app/App.ts
@@ -202,25 +202,3 @@ export class App {
     return false;
   }
 }
-
-(async function () {
-  console.log(`PowerPool Agent Node version: ${process.env.npm_package_version}`);
-  const app = new App();
-  await app.start();
-})().catch(error => {
-  console.error(error);
-  console.log('App.ts: Unexpected error. Stopping the app with a code (1).');
-  process.exit(1);
-});
-
-process.on('unhandledRejection', function (error: Error, _promise) {
-  const msg = `Unhandled Rejection, reason: ${error}`;
-  console.log(error.stack);
-
-  if (unhandledExceptionsStrictMode) {
-    console.log('Stopping the app with a code (1) since the "unhandledExceptionsStrictMode" is ON.');
-    process.exit(1);
-  }
-
-  console.log(msg);
-});
