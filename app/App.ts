341def3598e74980c89dbce1ac17591ea6d7b49e
defi-dev <defi.dev@protonmail.com>
2023-07-26 06:31:11 +0800

    move config to Cli.ts

--- app/App.ts
+++ app/App.ts
@@ -1,16 +1,7 @@
-import * as fs from 'fs';
-import * as path from 'path';
-import YAML from 'yamljs';
-
-import { Config, AllNetworksConfig, NetworkConfig, AgentConfig } from './Types';
+import { Config, AllNetworksConfig } from './Types';
 import { Network } from './Network.js';
 import { nowTimeString } from './Utils.js';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 import { initApi } from './Api.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 function clog(...args: any[]) {
   console.log(`>>> ${nowTimeString()} >>> App:`, ...args);
@@ -22,74 +13,8 @@ export class App {
 
   public unhandledExceptionsStrictMode = false;
 
-  constructor() {
+  constructor(config: Config) {
     this.networks = {};
-    let config: Config;
-
-    if (!process.env.NETWORK_NAME) {
-      const configName = process.argv[2] ? process.argv[2].trim() : 'main';
-      console.log(`Reading configuration from ./config/${configName}.yaml ...`);
-      config = YAML.parse(
-        fs.readFileSync(path.resolve(__dirname, `../config/${configName}.yaml`)).toString(),
-      ) as Config;
-    } else {
-      console.log('NETWORK_NAME is found. Assuming configuration is done with ENV vars...');
-      const networkName = process.env.NETWORK_NAME;
-      const networkRpc = process.env.NETWORK_RPC;
-      const agentAddress = process.env.AGENT_ADDRESS;
-      const dataSource = process.env.DATA_SOURCE;
-      const graphUrl = process.env.GRAPH_URL;
-      const keeperAddress = process.env.KEEPER_WORKER_ADDRESS;
-      const keyPassword = process.env.KEYPASSWORD || '';
-      const acceptMaxBaseFeeLimit = process.env.ACCEPT_MAX_BASE_FEE_LIMIT === 'true';
-      const accrueReward = process.env.ACCRUE_REWARD === 'true';
-      const api = process.env.API_SERVER === 'true';
-      if (!networkRpc) {
-        throw new Error('ENV Config: Missing NETWORK_RPC value');
-      }
-      if (!agentAddress) {
-        throw new Error('ENV Config: Missing AGENT_ADDRESS value');
-      }
-      if (!keeperAddress) {
-        throw new Error('ENV Config: Missing KEEPER_ADDRESS value');
-      }
-      if (!keyPassword) {
-        throw new Error('ENV Config: Missing KEYPASSWORD value');
-      }
-      if (dataSource === 'subgraph' && !graphUrl) {
-        throw new Error('ENV CONFIG: On order to use subgraph as data source, you must define GRAPH_URL');
-      }
-      const agentConfig: AgentConfig = {
-        accept_max_base_fee_limit: acceptMaxBaseFeeLimit,
-        accrue_reward: accrueReward,
-        executor: 'pga',
-        keeper_worker_address: keeperAddress,
-        key_pass: keyPassword,
-        data_source: dataSource,
-        graph_url: graphUrl,
-      };
-
-      const netConfig: NetworkConfig = {
-        rpc: process.env.NETWORK_RPC,
-        agents: {
-          [agentAddress]: agentConfig,
-        },
-      };
-
-      config = {
-        api,
-        strict: {
-          all: false,
-        },
-        networks: {
-          enabled: [networkName],
-          details: {
-            [networkName]: netConfig,
-          },
-        },
-        observe: false,
-      };
-    }
 
     if (!!config.api) {
       let port = 8099;
